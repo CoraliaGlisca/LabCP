03a630ca4d5286010d74afb780524356d17c5ac2
CoraliaGlisca <32877341+CoraliaGlisca@users.noreply.github.com>
2018-01-16 21:56:48 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,s;
+    for(i=1;i<=100;i++)
+    {
+        for(i=32;i<=39;i++)
+            printf("%d\n", i);
+            break;
+    }
+    return 0;
+}
